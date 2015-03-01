a076a1f07894f884a4c9bf66d79ca4486b55ebd3
sergigalceran <sergi_galce7@hotmail.com>
2015-03-01 19:51:23 +0100

    Ejercicios Java

---
+++ ejerciciosjava/src/main/java/edu/uoc/eetac/dsa/sgalceran/servicios_concurrentes/Servidor.java
@@ -0,0 +1,5 @@
+package edu.uoc.eetac.dsa.sgalceran.servicios_concurrentes;
+
+public class Servidor {
+
+}
--- ejerciciosjava/src/main/java/edu/upc/eetac/dsa/sgalceran/threads/Principal.java
+++ ejerciciosjava/src/main/java/edu/upc/eetac/dsa/sgalceran/threads/Principal.java
@@ -2,13 +2,13 @@ package edu.upc.eetac.dsa.sgalceran.threads;
 
 public class Principal {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 	
 		runnable r = new runnable();
         thread1 thread = new thread1();
         r.run();
         thread.start();
-        r.join();
+        //r.join();
         thread.join();	
 	}
 	
